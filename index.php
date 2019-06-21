174ee6a4891d05faf39ae928ddb7a0ae86db083d
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-21 17:57:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -46,13 +46,13 @@ else
 	echo "Method not allowed";
 }
 
-	$hostname= "138.197.63.247";
+	$servername= "nice-fireant-64.localtunnel.me";
 $username = "root";
 $password = "";
 $dbname = "test";
 
 // Create connection
-$conn = new mysqli($hostname, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
