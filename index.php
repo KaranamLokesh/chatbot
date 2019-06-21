917adfa3b8e5315739651ba40d3ce0850f3adc55
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-21 12:48:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+include_once('conn.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -10,30 +10,7 @@ if($method == 'POST'){
 
 
 	$text = $json->result->parameters->designation;
-	$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nightDuty";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-$sql = "SELECT * from Timesheet";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
- // output data of each row
- while($row = mysqli_fetch_assoc($result)) {
- echo $result;
- }
-} else {
- echo "0 results";
-}
+	
 
 
 	switch ($text) {
