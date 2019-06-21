ab28f45ef6a96edc4b7b981851b435f9942e2401
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-21 15:23:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,50 @@
 
 
 <?php
-include_once('conn.php');
+<?php 
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+
+	$text = $json->result->parameters->designation;
+
+
+
+	switch ($text) {
+		case 'designation':
+			echo ($result);
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
 	$servername = "192.168.235.115";
 $username = "Prospecta";
 $password = "";
