36e093181a0156856843bfcec46569277d6a1c94
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-21 13:07:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("connection failed: " . $conn->connect_error);
+    die( $conn->connect_error);
 } 
 echo "Connected successfully";
 $sql = "SELECT * from Timesheet";
