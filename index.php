124c6f8c08aafa85f4618ce11a3052d833b0e7c4
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-25 15:20:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,56 @@
 	$text = $json->queryResult->parameters->designation;
 
 
+$serverName = "182.75.89.80,5180\\sqlexpress"; 
+
+$uid = "lokesh";   
+
+$pwd = "welcome1#";  
+
+$databaseName = "AviatorSMSTesting"; 
+
+
+
+$connectionInfo = array( "UID"=>$uid,                            
+
+                         "PWD"=>$pwd,                            
+
+                         "Database"=>$databaseName); 
+
+
+
+/* Connect using SQL Server Authentication. */  
+
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+
+
+
+
+$tsql = "SELECT smallint_205_ME FROM TRANS_INCIDENT_INCIDENT_IDENTIFICATIONS";  
+
+
+
+/* Execute the query. */  
+
+
+
+$stmt = sqlsrv_query( $conn,$tsql); 
+if ($stmt) {
+
+ 
+	while($row = sqlsrv_fetch_array($stmt)) {
+        echo $row["smallint_205_ME"];
+
+    }
+
+
+     echo "Statement executed.<br>\n";
+     
+
+
+
+} 
 
 
 
