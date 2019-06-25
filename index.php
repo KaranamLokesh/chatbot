7f5a2033f008db01299a7bd72619005e98332e1f
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-06-25 11:32:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,9 +29,12 @@ $database="test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
+	if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 	$sql = "SELECT * FROM test";
 $result = $conn->query($sql);
-	$conn->close();
+	
 
 
 
@@ -53,6 +56,7 @@ $result = $conn->query($sql);
 			$speech = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
+	$conn->close();
 
 	$response = new \stdClass();
 	$response->speech = $speech;
